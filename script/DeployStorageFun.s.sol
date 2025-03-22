6459ae28df9b5ea2ba3896dde20b6b37cf3698ea
Sarthak <sarthakbansalog@gmail.com>
2025-03-22 23:23:29 +0530

    Adding deploying script

--- script/DeployStorageFun.s.sol
+++ script/DeployStorageFun.s.sol
@@ -14,6 +14,20 @@ contract DeployFunWithStorage is Script {
         return (funWithStorage);
     }
 
-    
+    function printStorageData(address contractAddress) public view {
+        for (uint256 i = 0; i < 10; i++) {
+            bytes32 value = vm.load(contractAddress, bytes32(i));
+            console.log("Value at location", i, ":");
+            console.logBytes32(value);
+        }
+    }
+
+    function printFirstArrayElement(address contractAddress) public view {
+        bytes32 arrayStorageSlotLength = bytes32(uint256(2));
+        bytes32 firstElementStorageSlot = keccak256(abi.encode(arrayStorageSlotLength));
+        bytes32 value = vm.load(contractAddress, firstElementStorageSlot);
+        console.log("First element in array:");
+        console.logBytes32(value);
+    }
 
 }
