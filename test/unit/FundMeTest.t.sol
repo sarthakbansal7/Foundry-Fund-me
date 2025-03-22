ca1b5f35e0a9db7db649dc2612a61dcb5ec29eca
Sarthak <sarthakbansalog@gmail.com>
2025-03-22 23:25:20 +0530

    Adding test

---
+++ test/unit/FundMeTest.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {HelperConfig, CodeConstants} from "../../script/HelperConfig.s.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
+import {MockV3Aggregator} from "../mock/MockV3Aggregator.sol";
+
+contract FundMeTest is ZkSyncChainChecker, CodeConstants, StdCheats, Test {
+    FundMe public fundMe;
+    HelperConfig public helperConfig;
+
+    uint256 public constant SEND_VALUE = 0.1 ether; 
+    uint256 public constant STARTING_USER_BALANCE = 10 ether;
+    uint256 public constant GAS_PRICE = 1;
+
+    address public constant USER = address(1);
+
+    // uint256 public constant SEND_VALUE = 1e18;
+    // uint256 public constant SEND_VALUE = 1_000_000_000_000_000_000;
+    // uint256 public constant SEND_VALUE = 1000000000000000000;
+
+    function setUp() external {
+        if (!isZkSyncChain()) {
+            DeployFundMe deployer = new DeployFundMe();
+            (fundMe, helperConfig) = deployer.deployFundMe();
+        } else {
+            MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+            fundMe = new FundMe(address(mockPriceFeed));
+        }
+        vm.deal(USER, STARTING_USER_BALANCE);
+    }
+
+    
+}
