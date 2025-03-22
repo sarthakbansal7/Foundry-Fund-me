3b7cb50b56c0e3465cef1f2f5f73d8c826b3c97d
Sarthak <sarthakbansalog@gmail.com>
2025-03-22 23:21:15 +0530

    Adding deploying script

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract DeployFundMe is Script {
+    
+}
