27f010f57beeae1f77cd6b3e14cf8bb52a31ea22
Sarthak <sarthakbansalog@gmail.com>
2025-03-23 08:57:55 +0530

    testing

---
+++ test/integration/InteractionsTest.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
+
+contract InteractionsTest is ZkSyncChainChecker, StdCheats, Test {
+    
+}
