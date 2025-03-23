e2a2b0bbf6c521f4660fb0f57235d1e8737c8e1d
Sarthak <sarthakbansalog@gmail.com>
2025-03-23 09:00:45 +0530

    Adding WithdrawFundMe to interactions.s.sol

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -21,4 +21,16 @@ contract FundFundMe is Script {
     }
 }
 
+contract WithdrawFundMe is Script {
+    function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        vm.stopBroadcast();
+        console.log("Withdraw FundMe balance!");
+    }
 
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        withdrawFundMe(mostRecentlyDeployed);
+    }
+}
