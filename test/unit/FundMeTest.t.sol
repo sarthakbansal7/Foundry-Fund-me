1a0d9714803810686766f8274eadceeb8d6e0b7a
Sarthak <sarthakbansalog@gmail.com>
2025-03-23 08:45:59 +0530

    Adding test testWithdrawFromMultipleFunders

--- test/unit/FundMeTest.t.sol
+++ test/unit/FundMeTest.t.sol
@@ -106,4 +106,24 @@ contract FundMeTest is ZkSyncChainChecker, CodeConstants, StdCheats, Test {
     }
 
     
+    function testWithdrawFromMultipleFunders() public funded skipZkSync{
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 2;
+        for (uint160 i = startingFunderIndex; i < numberOfFunders + startingFunderIndex; i++) {
+            // prank + deal
+            hoax(address(i), STARTING_USER_BALANCE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        assert(address(fundMe).balance == 0);
+        assert(startingFundMeBalance + startingOwnerBalance == fundMe.getOwner().balance);
+        assert((numberOfFunders + 1) * SEND_VALUE == fundMe.getOwner().balance - startingOwnerBalance);
+    }
 }
